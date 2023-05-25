15fc51d1febf2ad277e8b7dca8896b1b67cde972
allredty <all21059@byui.edu>
2023-05-25 10:47:38 -0600

    added server file

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const app = express();
+
+app.get("/tyler", (req, res)=>{
+    res.send("Hello Tyler! You LOSER!!!")
+})
+
+
+app.get("/", (req, res)=>{
+    
+    res.send("Welcome to my backend API! Loser!!!")
+})
+
+app.listen(3000, ()=> {
+
+    console.log("Listening");
+})
