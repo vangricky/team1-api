d2de1dadc9d597ea2d7a87b50f96026c09530062
Ricky Vang <vangcheemeng@yahoo.com>
2023-05-30 11:20:27 -0600

    edit ricky

--- test/ricky.test.js
+++ test/ricky.test.js
@@ -1,5 +1,8 @@
-const response = await fetch('http://localhost:3000/ricky');
+it("Should test local host name", async()=>{
+    const response = await fetch('http://localhost:3000/ricky');
 
-const name = await response.text();
+    const name = await response.text();
+    
+    expect(name).toBe("ricky")
+})
 
-expect(name).toBe("ricky")
